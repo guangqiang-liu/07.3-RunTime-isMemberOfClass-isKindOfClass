74293d5e02583fed5c126aa5852fed4f22bc3f54
刘光强 <1126756952@qq.com>
2020-06-21 17:39:42 +0800

    初始化代码仓库

---
+++ 07.3-Runtime isMemberOfClass isKindOfClass/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  07.3-Runtime isMemberOfClass isKindOfClass
+//
+//  Created by 刘光强 on 2020/2/8.
+//  Copyright © 2020 guangqiang.liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
--- 07.3-Runtime isMemberOfClass isKindOfClass/main.m
+++ 07.3-Runtime isMemberOfClass isKindOfClass/main.m
@@ -7,11 +7,33 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Person.h"
+#import <objc/runtime.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        Person *person = [[Person alloc] init];
+        
+//        NSLog(@"%d", [person isMemberOfClass:[Person class]]); // 1
+//        NSLog(@"%d", [person isMemberOfClass:[NSObject class]]); // 0
+//
+//        NSLog(@"%d", [person isKindOfClass:[Person class]]); // 1
+//        NSLog(@"%d", [person isKindOfClass:[NSObject class]]); // 1
+        
+        NSLog(@"%d", [Person isMemberOfClass:[Person class]]); // 0
+        NSLog(@"%d", [Person isKindOfClass:[Person class]]); // 0
+        
+        NSLog(@"%d", [Person isMemberOfClass:object_getClass([Person class])]); // 1
+        
+        NSLog(@"%d", [Person isKindOfClass:object_getClass([Person class])]); // 1
+        
+        NSLog(@"%d", [Person isKindOfClass:object_getClass([NSObject class])]); // 1
+        
+        
+        NSLog(@"%d", [[NSObject class] isKindOfClass:[NSObject class]]); // 1
+        
+        NSLog(@"%d", [[Person class] isKindOfClass:[NSObject class]]); // 1
     }
     return 0;
 }
